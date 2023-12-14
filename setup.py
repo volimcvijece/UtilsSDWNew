12203582f995586e953fa3f5bb18d5e45b7b9e02
TC <caric.tonko@gmail.com>
2023-12-14 09:13:59 +0100

    fix:setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
     name='UtilsSDW',
-    version='0.0.4',
+    version='0.0.5',
     description='Utility functions for dealing with SDW specific tables (dlookup etc)',
     url='git@github.com:volimcvijece/UtilsSDW.git',
     author='Tonko Caric',
     author_email='caric.tonko@gmail.com',
     license='unlicense',
-    packages=['utilssdw'], # #packages=find_packages()
+    packages=find_packages(),
+    #packages=['utilssdw'], # #packages=find_packages()
     # Needed for dependencies
     install_requires=['pandas'], #no nr - any version. specify - "numpy>=1.13.3"
     zip_safe=False
